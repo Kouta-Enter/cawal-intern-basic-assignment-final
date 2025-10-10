6f8597b670b7f7cbef4a58bc3bfc220838d4b555
Kouta-Enter <kouta2006n@gmail.com>
2025-10-10 08:37:28 +0000

    見た目を整えた。

--- task-manager/task-app/frontend/src/App.tsx
+++ task-manager/task-app/frontend/src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+type a_data = { completed: number; id: number; title: string };
+type datas = a_data[];
+const appdata: datas = [
+  { completed: 0, id: 1, title: 'task1' },
+  { completed: 0, id: 2, title: 'task2' },
+];
+function getdata(){
+  let url: string= `https//:${location.host}/`
+  fetch(url) // リクエストを送信
+	  .then((response) => response.json())
+	  .then((data) => {
+	  // APIレスポンスを用いた処理
+	  console.log(data);
+});
+}
+function sortById(a: a_data, b: a_data) {
+  return a.id - b.id;
+}
+appdata.sort(sortById);
+function sortByCompleted(a: a_data, b: a_data) {
+  return a.completed - b.completed;
+}
+appdata.sort(sortByCompleted);
+function CheckComplete(){
+  return <input
+  type="checkbox"
+  defaultChecked={false}/>
+}
 
+function Deletebutton(){
+  return  <button className="delbutton">
+  delete
+</button>
+}
+function Oneline({prop}){
+  const [editting,setEditting] =useState(false)
+  function editting_func(){
+    setEditting(!editting);
+  }
+  return <>
+    {!editting && <><CheckComplete/>{prop.title}
+  <button className="edit-add" onClick={editting_func}>
+  Edit
+</button><Deletebutton/></>}
+{editting && <><input type="text" defaultValue={prop.title}/>
+<button className="edit-add" type="submit" onClick={editting_func}>
+  Enter
+</button>
+  <button className="cancel" onClick={editting_func}>
+  Cancel
+</button></>}</>
+
+ 
+}
+function TaskList() {
+  const list = appdata.map((task) => <h2><Oneline prop={task}/></h2>);
+  return <div>{list}</div>;
+}
+function AddForm(){
+  return <h2 className="adds"><label><input type="text" defaultValue="add new task"/><button className="edit-add"> Enter </button></label></h2>
+}
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Task-manager</h1>
+      <TaskList />
+      <AddForm/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
